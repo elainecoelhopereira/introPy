34cf5ed28abd674c70666fe9abc301efd4822a5f
elainecoelhopereira <elainecoelho2018@gmail.com>
2025-11-13 17:09:17 -0300

    Alterações da 2 aula do curso com a estrutura do While, e uso do If-Elif-Else como um Smith-Case ou Match-Case

--- mainA.py
+++ mainA.py
@@ -1,12 +1,12 @@
-# 1- imports / biblioteca
+# 1 - imports / bibliotecas
 
-# 2- Classe
+# 2 - Classe
 
-# 3- Métodos e funções
-#def = definition = definição
+# 3 -  Métodos e Funções
+# def = definition = definição
 def print_hi(name):
-    print(f'Oi,{name}')  # a partir do Python 3
-    print('Oi,'+ name)  # antes do Python 3
+    print(f'Oi, {name}') # a partir do Python 3
+    print('Oi, ' + name) # antes do Python 3
 
 def calcular_area_do_retangulo(largura, comprimento):
     return largura * comprimento
@@ -18,25 +18,25 @@ def calcular_area_do_triangulo(largura, comprimento):
     return largura * comprimento / 2
 
 def contagem_progressiva(fim):
-    for numero in range(fim):  # repetir o bloco de zero até o fim
-        print(numero) # exibir o numero
+    for numero in range(fim): # repetir o bloco de zero até o fim
+        print(numero)         # exibir o número
 
 def apoiar_candidato(nome, vezes):
     for numero in range(vezes):
-        #contador = numero + 1 #contador recebe numero+1
-        #print(f'{contador} - {nome}')
+        # contador = numero + 1
+        # print(f'{contador} - {nome}')
 
-        if numero < 9: # acrescentar 0 na numeração de 1 a 9 p ficar alinhado
+        if numero < 9:
             print(f'00{numero + 1} - {nome}')
         elif numero < 99:
             print(f'0{numero + 1} - {nome}')
         else:
-            print(numero + 1, '-', nome) #pode retirar as linhas 26 e 27 e substitui por essa linha de codigo
+            print(numero + 1,'-', nome)
 
 def brincar_de_plim(fim):
     for numero in range(fim):
         if numero % 4 == 0:
-            print('PLIM')
+            print('PLIM!')
         else:
             print('{:0>3}'.format(numero))
 
@@ -53,41 +53,41 @@ def exibir_dia_da_semana_if(numero):
     elif numero == 5:
         print('O dia é sexta')
     elif numero == 6:
-        print('O dia é sabado')
+        print('O dia é sábado')
     elif numero == 7:
         print('O dia é domingo')
     else:
         print('Número de dia inválido. Digite um número de 1 a 7')
 
-#
-# def exibir_dia_da_semana_match(numero):
-#     print("Execução com MATCH")
-#
-#     match numero:
-#         case 1:
-#             print('O dia é segunda')
-#             exit()
-#         case 2:
-#             print('O dia é terça')
-#             exit()
-#         case 3:
-#             print('O dia é quarta')
-#             exit()
-#         case 4:
-#             print('O dia é quinta')
-#             exit()
-#         case 5:
-#             print('O dia é sexta')
-#             exit()
-#         case 6:
-#             print('O dia é sabado')
-#             exit()
-#         case 7:
-#             print('O dia é domingo')
-#             exit()
-#         case _:   #nesse case o _ é p outros valores q sejam digitados
-#             print('Número de dia inválido. Digite um número de 1 a 7') #
+'''
+def exibir_dia_da_semana_match(numero):
+    print("Execução com MATCH")
 
+    match numero:
+        case 1:
+            print('O dia é segunda')
+            exit()
+        case 2:
+            print('O dia é terça')
+            exit()
+        case 3:
+            print('O dia é quarta')
+            exit()
+        case 4:
+            print('O dia é quinta')
+            exit()
+        case 5:
+            print('O dia é sexta')
+            exit()
+        case 6:
+            print('O dia é sábado')
+            exit()
+        case 7:
+            print('O dia é domingo')
+            exit()
+        case _:
+            print('Número de dia inválido. Digite um número de 1 a 7')
+'''
 
 def brincar_de_para_ou_continua():
     resposta = 'C' # S aqui significa que continua
